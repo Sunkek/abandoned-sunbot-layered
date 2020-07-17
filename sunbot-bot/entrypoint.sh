41b1352186db1081d5e0e8e28b71674e39a179e6
“Suncake” <xexc@mail.ru>
2020-07-17 16:26:12 +0500

    Install netcat, wait for API

---
+++ sunbot-bot/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update && apt-get install -y netcat
+echo "Waiting for API..."
+  while ! nc -z $API_HOST $API_PORT; do
+    sleep 0.1
+    done
+  echo "PostgreSQL started"
+fi
+
+exec "$@"
