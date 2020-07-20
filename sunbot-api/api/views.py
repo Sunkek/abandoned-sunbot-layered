0e37f764dca8d0125c8fd8ce2ad7eaff6980727b
“Suncake” <xexc@mail.ru>
2020-07-20 13:25:16 +0500

    Add debug print

--- sunbot-api/api/views.py
+++ sunbot-api/api/views.py
@@ -47,49 +47,6 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class MessagesViewSet(viewsets.ModelViewSet):
-    queryset = Messages.objects.all()
-    serializer_class = MessagesSerializer
-
-    def partial_update(self, request, *args, **kwargs):
-        """PATCH requests fall here.
-        Here I'm working from the idea that the entry already exists,
-        so I just find it and update the counters. Only if it doesn't exist,
-        I create a new one and try to save it. If it can't save because
-        there's no member in the database, I create that member."""
-        data = request.data
-        try:
-            # Find the existing message entry
-            messages = Messages.objects.get(
-                guild_id=data["guild_id"],
-                channel_id=data["channel_id"],
-                user_id=User(user_id=data["user_id"]),
-                period=data["period"][:-2]+"01",  # The first of the current month
-            )
-        except ObjectDoesNotExist:
-            # Entry not found - create one!
-            messages = Messages(
-                guild_id=data["guild_id"],
-                channel_id=data["channel_id"],
-                user_id=User(user_id=data["user_id"]),
-                period=data["period"][:-2]+"01",  # The first of the current month
-            )
-        # Update counters
-        messages.postcount += data["postcount"]
-        messages.attachments += data["attachments"]
-        messages.words += data["words"]
-        try:
-            # Submit changes
-            messages.save()
-        except IntegrityError:
-            # If there's no member - create one!
-            author = User(user_id=request.data["user_id"])
-            author.save()
-            messages.save()
-        serializer = self.get_serializer(messages)
-        return Response(serializer.data)
-
-
 class SettingsViewSet(viewsets.ModelViewSet):
     queryset = Guild.objects.all()
     serializer_class = GuildSerializer
@@ -138,13 +95,61 @@ class BirthdaysTodayViewSet(viewsets.ModelViewSet):
         return Response(birthdays)
 
 
+class MessagesViewSet(viewsets.ModelViewSet):
+    queryset = Messages.objects.all()
+    serializer_class = MessagesSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        """PATCH requests fall here.
+        Here I'm working from the idea that the entry already exists,
+        so I just find it and update the counters. Only if it doesn't exist,
+        I create a new one and try to save it. If it can't save because
+        there's no member in the database, I create that member."""
+        data = request.data
+        print(data)
+        try:
+            # Find the existing message entry
+            messages = Messages.objects.get(
+                guild_id=data["guild_id"],
+                channel_id=data["channel_id"],
+                user_id=User(user_id=data["user_id"]),
+                period=data["period"][:-2]+"01",  # The first of the current month
+            )
+            print("Found")
+            print(messages)
+        except ObjectDoesNotExist:
+            # Entry not found - create one!
+            messages = Messages(
+                guild_id=data["guild_id"],
+                channel_id=data["channel_id"],
+                user_id=User(user_id=data["user_id"]),
+                period=data["period"][:-2]+"01",  # The first of the current month
+            )
+            print("Created")
+            print(messages)
+        # Update counters
+        messages.postcount += data["postcount"]
+        messages.attachments += data["attachments"]
+        messages.words += data["words"]
+        try:
+            # Submit changes
+            messages.save()
+            print("Saved")
+        except IntegrityError:
+            # If there's no member - create one!
+            author = User(user_id=request.data["user_id"])
+            author.save()
+            print("Created author")
+            messages.save()
+            print("Saved")
+        serializer = self.get_serializer(messages)
+        print(serializer.data)
+        return Response(serializer.data)
+
+
 """Define the allowed request methods for each ModelViewSet"""
 user = UserViewSet.as_view({
     'get': 'retrieve',
-    'patch': 'partial_update',
-})
-messages = MessagesViewSet.as_view({
-    'get': 'list',
     'patch': 'partial_update',
 })
 settings = SettingsViewSet.as_view({
@@ -153,4 +158,8 @@ settings = SettingsViewSet.as_view({
 })
 birthdays_today = BirthdaysTodayViewSet.as_view({
     'get': 'list',
+})
+messages = MessagesViewSet.as_view({
+    'get': 'list',
+    'patch': 'partial_update',
 })
