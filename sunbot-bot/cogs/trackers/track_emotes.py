419133e56f2008e5e726770782efdd3129416068
“Suncake” <xexc@mail.ru>
2020-07-21 16:49:30 +0500

    Fix regexp

--- sunbot-bot/cogs/trackers/track_emotes.py
+++ sunbot-bot/cogs/trackers/track_emotes.py
@@ -8,24 +8,20 @@ import re
 
 from utils import rest_api
 
-class TrackEmoji(commands.Cog):
+class TrackEmotes(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message):
         # I don't want to save info about DMs and webhooks
-        print("Emoji check...")
         if message.guild and message.guild.get_member(message.author.id):
-            print("...in guild and not webhook...")
             if self.bot.settings.get(message.guild.id, {}).get("track_emotes"):
-                print("...guild track emotes on...")
-                p = re.compile("<:[\w]:[\d]>")
-                emoji = p.match(message.content)
+                p = re.compile("<:[\w+]:[\d+]>")
+                emoji = p.findall(message.content)
                 print(message.content)
                 print(emoji)
                 if emoji:
-                    print(emoji)
                     """await rest_api.add_emotes(
                         self.bot, 
                         guild_id=message.guild.id,
@@ -39,4 +35,4 @@ class TrackEmoji(commands.Cog):
         
 
 def setup(bot):
-    bot.add_cog(TrackEmoji(bot))
+    bot.add_cog(TrackEmotes(bot))
