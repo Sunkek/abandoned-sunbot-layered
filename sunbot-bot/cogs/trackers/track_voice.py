724a962cf6ae47408e0ce05d333c5bdb4e3da7ae
“Suncake” <xexc@mail.ru>
2020-07-21 16:34:37 +0500

    Don't save if there's just one member in voice

--- sunbot-bot/cogs/trackers/track_voice.py
+++ sunbot-bot/cogs/trackers/track_voice.py
@@ -20,7 +20,8 @@ class TrackVoice(commands.Cog):
         for guild in self.bot.guilds:
             if self.bot.settings.get(guild.id, {}).get("track_voice"):
                 for channel in guild.voice_channels:
-                    if channel.members:
+                    # Should be more than 1 member in voice
+                    if channel.members and len(channel.members) > 1:
                         for member in channel.members:
                             await rest_api.add_voice(
                                 self.bot, 
