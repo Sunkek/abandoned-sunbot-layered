f5c894604359bca8aab5a68a0bd186efec9b6a86
“Suncake” <xexc@mail.ru>
2020-07-23 12:52:03 +0500

    Override the basic get_paginated_response

--- sunbot-api/api/serializers.py
+++ sunbot-api/api/serializers.py
@@ -55,12 +55,12 @@ class EmotesSerializer(serializers.ModelSerializer):
 
 
 class CustomPageNumberPagination(pagination.PageNumberPagination):
-    
+
     def get_paginated_response(self, data, total):
         return Response({
             "next": self.get_next_link(),
             "previous": self.get_previous_link(),
-            "current": self.self.page.number,
+            "current": self.page.number,
             "last": self.page.paginator.num_pages,
             "count": self.page.paginator.count,
             "total": total,
