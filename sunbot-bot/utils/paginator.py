dd663366f430d1a44473d85064b828a7f8b5a4b8
“Suncake” <xexc@mail.ru>
2020-07-24 10:07:53 +0500

    Refactor into separate pagination file

--- sunbot-bot/utils/paginator.py
+++ sunbot-bot/utils/paginator.py
@@ -28,24 +28,20 @@ async def paginate(ctx, message, data, headers, footers):
             for future in pending: future.cancel()
             break
 
+        # First page
         if str(payload.emoji) == "⏮️" and data["current"] != 1:
             url = data["next"] or data["previous"]
             url = url.split("?")[0] + "?page=1"
-            top_chart = await rest_api.send_get(
-                ctx.bot, url, 
+            data = await rest_api.send_get(
+                ctx.bot, url,
                 guild_id=ctx.guild.id, channel_id=ctx.kwargs.get("channel"),
             )
             
-            columns = await helpers.parse_top_json(top_chart, ctx)
+            columns = await helpers.parse_top_json(data, ctx)
             table = helpers.format_columns(
                 columns["counts"], columns["user_names"], 
                 headers=headers, footers=footers
             )
-            time_range = ctx.kwargs.get("time_range")
-            if ctx.kwargs.get("channel"):
-                channel = f"in {ctx.kwargs.get('channel').name}" 
-            else:
-                channel = ""
             embed = message.embeds[0]
             embed.description=f"`{table}`"
             embed.set_footer(
@@ -53,95 +49,60 @@ async def paginate(ctx, message, data, headers, footers):
             )
             await message.edit(embed=embed)
 
-
-
-
-
-            
-        elif str(payload.emoji) == "⏪" and top_chart["previous"]:
-            top_chart = await rest_api.send_get(
-                self.bot, 
-                top_chart["previous"], 
-                guild_id=ctx.guild.id,
-                channel_id=channel,
+        # Previous page            
+        elif str(payload.emoji) == "⏪" and data["previous"]:
+            data = await rest_api.send_get(
+                ctx.bot, data["previous"],
+                guild_id=ctx.guild.id, channel_id=ctx.kwargs.get("channel"),
             )
-            user_ids = [i["user_id"] for i in top_chart["results"]]
-            postcounts = [i["count"] for i in top_chart["results"]]
-            user_ids, postcounts = zip(*[
-                (i["user_id"], i["count"]) for i in top_chart["results"]
-            ])
-            user_ids = [
-                await helpers.get_member_name(
-                    self.bot, ctx.guild, i
-                ) for i in user_ids
-            ]
-            postcounts = list(postcounts)
-            headers = ["POSTCOUNT", "MEMBER"]
-            footers = [top_chart["total"], "TOTAL"]
+            columns = await helpers.parse_top_json(data, ctx)
             table = helpers.format_columns(
-                postcounts, user_ids, headers=headers, footers=footers
+                columns["counts"], columns["user_names"], 
+                headers=headers, footers=footers
             )
+            embed = message.embeds[0]
             embed.description=f"`{table}`"
             embed.set_footer(
-                text=f"Page {top_chart['current']}/{top_chart['last']}"
+                text=f"Page {data['current']}/{data['last']}"
             )
             await message.edit(embed=embed)
-        elif str(payload.emoji) == "⏩" and top_chart["next"]:
-            top_chart = await rest_api.send_get(
-                self.bot, 
-                top_chart["next"], 
-                guild_id=ctx.guild.id,
-                channel_id=channel,
+
+        # Next page
+        elif str(payload.emoji) == "⏩" and data["next"]:
+            data = await rest_api.send_get(
+                ctx.bot, data["next"],
+                guild_id=ctx.guild.id, channel_id=ctx.kwargs.get("channel"),
             )
-            user_ids = [i["user_id"] for i in top_chart["results"]]
-            postcounts = [i["count"] for i in top_chart["results"]]
-            user_ids, postcounts = zip(*[
-                (i["user_id"], i["count"]) for i in top_chart["results"]
-            ])
-            user_ids = [
-                await helpers.get_member_name(
-                    self.bot, ctx.guild, i
-                ) for i in user_ids
-            ]
-            postcounts = list(postcounts)
-            headers = ["POSTCOUNT", "MEMBER"]
-            footers = [top_chart["total"], "TOTAL"]
+            columns = await helpers.parse_top_json(data, ctx)
             table = helpers.format_columns(
-                postcounts, user_ids, headers=headers, footers=footers
+                columns["counts"], columns["user_names"], 
+                headers=headers, footers=footers
             )
+            embed = message.embeds[0]
             embed.description=f"`{table}`"
             embed.set_footer(
-                text=f"Page {top_chart['current']}/{top_chart['last']}"
+                text=f"Page {data['current']}/{data['last']}"
             )
             await message.edit(embed=embed)
-        elif str(payload.emoji) == "⏭️" and top_chart["current"] != top_chart["last"]:
-            url = top_chart["next"] or top_chart["previous"]
-            url = url.split("?")[0] + f"?page={top_chart['last']}"
-            top_chart = await rest_api.send_get(
-                self.bot, 
-                url, 
-                guild_id=ctx.guild.id,
-                channel_id=channel,
+        
+        # Last page
+        elif str(payload.emoji) == "⏭️" and data["current"] != data["last"]:
+            url = data["next"] or data["previous"]
+            url = url.split("?")[0] + f"?page={data['last']}"
+            data = await rest_api.send_get(
+                ctx.bot, url,
+                guild_id=ctx.guild.id, channel_id=ctx.kwargs.get("channel"),
             )
-            user_ids = [i["user_id"] for i in top_chart["results"]]
-            postcounts = [i["count"] for i in top_chart["results"]]
-            user_ids, postcounts = zip(*[
-                (i["user_id"], i["count"]) for i in top_chart["results"]
-            ])
-            user_ids = [
-                await helpers.get_member_name(
-                    self.bot, ctx.guild, i
-                ) for i in user_ids
-            ]
-            postcounts = list(postcounts)
-            headers = ["POSTCOUNT", "MEMBER"]
-            footers = [top_chart["total"], "TOTAL"]
+            columns = await helpers.parse_top_json(data, ctx)
             table = helpers.format_columns(
-                postcounts, user_ids, headers=headers, footers=footers
+                columns["counts"], columns["user_names"], 
+                headers=headers, footers=footers
             )
+            embed = message.embeds[0]
             embed.description=f"`{table}`"
             embed.set_footer(
-                text=f"Page {top_chart['current']}/{top_chart['last']}"
+                text=f"Page {data['current']}/{data['last']}"
             )
             await message.edit(embed=embed)
+
         for future in pending: future.cancel()
