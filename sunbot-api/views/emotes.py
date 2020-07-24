6cdae0463c3202275074b7b3723cf39db046f0fe
“Suncake” <xexc@mail.ru>
2020-07-24 16:20:58 +0500

    Add raw query

--- sunbot-api/views/emotes.py
+++ sunbot-api/views/emotes.py
@@ -84,7 +84,7 @@ class TopEmotesViewSet(viewsets.ModelViewSet):
                 "SELECT emotes.emote as emote, sum(emotes.count) as message_count, "
                 "sum(reactions.count) as reaction_count, "
                 "sum(emotes.count)+sum(reactions.count) as total_count "
-                "FROM emotes JOIN reactions ON emotes.emote = reactions.emote "
+                "FROM emotes JOIN reactions ON (emotes.emote = reactions.emote "
                 "AND emotes.period = reactions.period AND "
                 "emotes.guild_id = reactions.guild_id) "
                 f"WHERE emotes.guild_id={data['guild_id']} OR "
