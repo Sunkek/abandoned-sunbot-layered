27e80077226e5b60a62cea497a5edb07efb65be2
“Suncake” <xexc@mail.ru>
2020-08-17 16:34:43 +0500

    Remove debug print, uncomment waiting for full hour

--- sunbot-bot/cogs/settings/set_advertising_reminder.py
+++ sunbot-bot/cogs/settings/set_advertising_reminder.py
@@ -95,55 +95,52 @@ class SetAdReminder(commands.Cog):
     @tasks.loop(hours=1.0)
     async def ad_reminder(self):
         """Remind server admins to advertise when it's allowed"""
-        try:
-            for guild, settings in self.bot.settings.items():
-                if settings["ad_reminder_channel_id"]:
-                    guild = self.bot.get_guild(int(guild))
-                    embed = discord.Embed(
-                        title="Advertising Reminder",
-                        color=guild.me.color
+        for guild, settings in self.bot.settings.items():
+            if settings["ad_reminder_channel_id"]:
+                guild = self.bot.get_guild(int(guild))
+                embed = discord.Embed(
+                    title="Advertising Reminder",
+                    color=guild.me.color
+                )
+                # Disboard - every 2 hours
+                if settings["ad_reminder_disboard"] and datetime.now().hour % 2 == 0:
+                    embed.add_field(
+                        name='Disboard',
+                        value=f'`every 2 hours`\nBump at [WEBSITE](https://disboard.org/server/{guild.id}) \nor with <@302050872383242240>:\n`!d bump`'
                     )
-                    # Disboard - every 2 hours
-                    if settings["ad_reminder_disboard"] and datetime.now().hour % 2 == 0:
-                        embed.add_field(
-                            name='Disboard',
-                            value=f'`every 2 hours`\nBump at [WEBSITE](https://disboard.org/server/{guild.id}) \nor with <@302050872383242240>:\n`!d bump`'
-                        )
-                    # Disforge - every 3 hours
-                    if settings["ad_reminder_disforge"] and datetime.now().hour % 3 == 0:
-                        embed.add_field(
-                            name='Disforge',
-                            value=f'`every 3 hours`\nBump at [WEBSITE](https://disforge.com/dashboard)'
-                        )
-                    # Discord.me
-                    if settings["ad_reminder_discordme"] and datetime.now().hour % 6 == 0:
-                        embed.add_field(
-                            name='Discord.me',
-                            value=f'`every 6 hours`\nBump at [WEBSITE](https://discord.me/dashboard)'
-                        )
-                    # discordservers
-                    if settings["ad_reminder_discordservers"] and datetime.now().hour % 12 == 0:
-                        embed.add_field(
-                            name="discordservers",
-                            value=f'`every 12 hours`\nBump at [WEBSITE](https://discordservers.com/panel/{guild.id}/bump)'
-                        )
+                # Disforge - every 3 hours
+                if settings["ad_reminder_disforge"] and datetime.now().hour % 3 == 0:
+                    embed.add_field(
+                        name='Disforge',
+                        value=f'`every 3 hours`\nBump at [WEBSITE](https://disforge.com/dashboard)'
+                    )
+                # Discord.me
+                if settings["ad_reminder_discordme"] and datetime.now().hour % 6 == 0:
+                    embed.add_field(
+                        name='Discord.me',
+                        value=f'`every 6 hours`\nBump at [WEBSITE](https://discord.me/dashboard)'
+                    )
+                # discordservers
+                if settings["ad_reminder_discordservers"] and datetime.now().hour % 12 == 0:
+                    embed.add_field(
+                        name="discordservers",
+                        value=f'`every 12 hours`\nBump at [WEBSITE](https://discordservers.com/panel/{guild.id}/bump)'
+                    )
 
-                    if embed.fields:
-                        role = guild.get_role(int(settings["ad_reminder_role_id"]))
-                        await guild.get_channel(
-                            int(settings["ad_reminder_channel_id"])
-                        ).send(content=role.mention if role else None, embed=embed)
-        except Exception as e:
-            print(e)
+                if embed.fields:
+                    role = guild.get_role(int(settings["ad_reminder_role_id"]))
+                    await guild.get_channel(
+                        int(settings["ad_reminder_channel_id"])
+                    ).send(content=role.mention if role else None, embed=embed)
 
-    '''@ad_reminder.before_loop
+    @ad_reminder.before_loop
     async def before_ad_reminder(self):
         """Sleeping until the full hour"""
         await self.bot.wait_until_ready()
         await sleep(5) # To make sure bot reads settings
         now = datetime.now()
         next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
-        await sleep((next_hour - now).total_seconds())'''
+        await sleep((next_hour - now).total_seconds())
 
 def setup(bot):
     bot.add_cog(SetAdReminder(bot))
