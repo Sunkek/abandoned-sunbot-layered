7ca506db5f884425efb5e0a3ad87df3e1cec2752
“Suncake” <xexc@mail.ru>
2020-08-20 17:34:13 +0500

    Add debug print

--- sunbot-bot/cogs/welcome.py
+++ sunbot-bot/cogs/welcome.py
@@ -15,16 +15,22 @@ class Welcome(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         """Sends the initial welcome message"""
-        channel = self.bot.settings.get(member.guild.id, {}).get("welcome_channel")
-        text = self.bot.settings.get(member.guild.id, {}).get("welcome_message")
-        embed = self.bot.settings.get(member.guild.id, {}).get("welcome_message_embed")
-        if channel and (text or embed):
-            channel = member.guild.get_channel(channel)
-            text = helpers.format_message(text, guild=member.guild, user=member)
-            embed = discord.Embed.from_dict(
-                helpers.format_message(embed, guild=member.guild, user=member)
-            )
-            await channel.send(content=text or None, embed=embed or None)
+        try:
+            channel = self.bot.settings.get(member.guild.id, {}).get("welcome_channel")
+            print(channel)
+            text = self.bot.settings.get(member.guild.id, {}).get("welcome_message")
+            print(text)
+            embed = self.bot.settings.get(member.guild.id, {}).get("welcome_message_embed")
+            print(embed)
+            if channel and (text or embed):
+                channel = member.guild.get_channel(channel)
+                text = helpers.format_message(text, guild=member.guild, user=member)
+                embed = discord.Embed.from_dict(
+                    helpers.format_message(embed, guild=member.guild, user=member)
+                )
+                await channel.send(content=text or None, embed=embed or None)
+        except Exception as e:
+            print(e)
             
     @commands.command(
         description="Displays the current welcome message", 
